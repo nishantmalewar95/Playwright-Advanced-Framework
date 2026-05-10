a70e9cc03e8aec3a723c298532dda34560e7abf1
Nishant Malewar <nishant.malewar@intellectdesign.com>
2026-05-10 17:14:25 +0530

    Cleanup: Removed untracked files and added .gitignore

--- tmp.py
+++
@@ -1,5 +0,0 @@
-import requests 
-r=requests.get('https://parabank.parasoft.com/parabank/requestloan.htm', timeout=20) 
-print(r.status_code) 
-text=r.text 
-print(text.find('id=\" "amount\')) ; echo print(text.find('name=\amount\')) ; echo print(text.find('id=\downPayment\')) ; echo print(text.find('id=\fromAccountId\')) ; echo print(text.find('Apply Now')) ; echo print(text.find('id=\loanStatus\')) ; echo idx=text.find('id=\amount\') ; echo print(text[max(0,idx-100):idx+300]) ; python tmp.py ; del tmp.py
